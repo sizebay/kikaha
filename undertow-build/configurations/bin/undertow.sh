a7e43bb9b133f1861c14cd1529d21b07c6f8e603
Miere Teixeira <miere00@gmail.com>
2014-03-21 14:36:41 -0300

    Improved startup script to be more configurable.

--- undertow-build/configurations/bin/undertow.sh
+++ undertow-build/configurations/bin/undertow.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 cd "`dirname $0`/.."
 
-# VARIABLES
+# CONFIGURABLE VARIABLES
 LIBDIR="./lib"
+APPDIR="./webapp"
+HOST=localhost
+PORT=9000
+
+# RUNTIME VARIABLES
 JAVA=java
 JAVA_OPTS=
 MAIN_CLASS=com.texoit.undertow.standalone.Main
 
+# READ CUSTOM CONFIGURATIONS
 if [ -e bin/undertow.conf ]; then
 	. bin/undertow.conf
 fi
 
 # MAIN
+JAVA_OPTS="-Dundertow.standalone.libdir=${LIBDIR} \
+	-Dundertow.standalone.appdir=${APPDIR} \
+	-Dundertow.standalone.host=${HOST} \
+	-Dundertow.standalone.port=${PORT} ${JAVA_OPTS}"
+
 CLASSPATH=`ls ${LIBDIR}/*.jar | tr '\n' ' ' | sed 's/  */ /g' | tr ' ' ':'`
+
 ${JAVA} ${JAVA_OPTS} -classpath "${CLASSPATH}" ${MAIN_CLASS}
