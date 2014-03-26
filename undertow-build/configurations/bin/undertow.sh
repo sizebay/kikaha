2d7b22fb7555a6e0695830b1e4c83be1645933cc
Miere Teixeira <miere00@gmail.com>
2014-03-26 18:09:41 -0300

    Improved class path configuration

--- undertow-build/configurations/bin/undertow.sh
+++ undertow-build/configurations/bin/undertow.sh
@@ -4,13 +4,14 @@ cd "`dirname $0`/.."
 # CONFIGURABLE VARIABLES
 LIBDIR="./lib"
 APPDIR="./webapp"
+WEBLIB="${APPDIR}/WEB-INF/lib"
+WEBCLASSES="${APPDIR}/WEB-INF/classes/"
 HOST=localhost
 PORT=9000
 
 # RUNTIME VARIABLES
 JAVA=java
 JAVA_OPTS=
-CLASSPATH="WEB-INF/lib"
 MAIN_CLASS=com.texoit.undertow.standalone.Main
 
 # READ CUSTOM CONFIGURATIONS
@@ -25,6 +26,8 @@ JAVA_OPTS="-Dundertow.standalone.libdir=${LIBDIR} \
 	-Dundertow.standalone.port=${PORT} ${JAVA_OPTS}"
 
 LIB_CLASSPATH=`ls ${LIBDIR}/*.jar | tr '\n' ' ' | sed 's/  */ /g' | tr ' ' ':'`
-CLASSPATH=${LIB_CLASSPATH}:${CLASSPATH}
+WEBLIB_CLASSPATH=`ls ${WEBLIB}/*.jar | tr '\n' ' ' | sed 's/  */ /g' | tr ' ' ':'`
+CLASSPATH=${LIB_CLASSPATH}:${WEBLIB_CLASSPATH}:${WEBCLASSES}
 
+echo "${JAVA} ${JAVA_OPTS} -classpath "${CLASSPATH}" ${MAIN_CLASS}"
 ${JAVA} ${JAVA_OPTS} -classpath "${CLASSPATH}" ${MAIN_CLASS}
