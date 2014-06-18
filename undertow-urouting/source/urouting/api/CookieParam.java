3ec9036c7223263d75f6fe2d8ff967cca9eb7717
Miere Teixeira <miere00@gmail.com>
2014-06-18 10:01:40 -0300

    Added parameter annotations that mimics JAXRS.

---
+++ undertow-urouting/source/urouting/api/CookieParam.java
@@ -0,0 +1,9 @@
+package urouting.api;
+
+import java.lang.annotation.*;
+
+@Target( { ElementType.PARAMETER } )
+@Retention( RetentionPolicy.RUNTIME )
+public @interface CookieParam {
+	String value();
+}
