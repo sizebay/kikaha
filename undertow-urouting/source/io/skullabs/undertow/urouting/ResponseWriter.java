c826b862ac3b9fcacc139cefb45ed44f313eb4b8
Miere Teixeira <miere00@gmail.com>
2014-06-18 10:04:00 -0300

    Added some helper methods It will make easy to send a response back while generating classes for routing methods.

--- undertow-urouting/source/io/skullabs/undertow/urouting/ResponseWriter.java
+++ undertow-urouting/source/io/skullabs/undertow/urouting/ResponseWriter.java
@@ -1,50 +1,100 @@
 package io.skullabs.undertow.urouting;
 
 import io.undertow.server.HttpServerExchange;
-import io.undertow.util.HeaderMap;
-import io.undertow.util.Headers;
-import io.undertow.util.HttpString;
+import io.undertow.util.*;
 
 import java.io.Writer;
 import java.nio.channels.Channels;
 
 import org.xnio.channels.StreamSinkChannel;
-
-import trip.spi.Provided;
-import trip.spi.Service;
-import trip.spi.ServiceProvider;
-import trip.spi.ServiceProviderException;
-import urouting.api.Header;
-import urouting.api.Response;
+import trip.spi.*;
+import urouting.api.*;
 import urouting.api.RoutingException;
 import urouting.api.Serializer;
 
+/**
+ * A helper class to write responses to the HTTP Client.
+ */
 @Service
 public class ResponseWriter {
 
-	@Provided ServiceProvider provider;
+	@Provided
+	ServiceProvider provider;
 
-	public void write( final HttpServerExchange exchange, final Response response )
-			throws ServiceProviderException, RoutingException {
-		exchange.setResponseCode( response.statusCode() );
-		sendHeaders(exchange, response);
-		serializeReponseEntity(exchange, response);
+	/**
+	 * Writes a response to HTTP Client informing that no content was available.
+	 * 
+	 * @param exchange
+	 */
+	public void write( final HttpServerExchange exchange ) {
+		sendStatusCode( exchange, 204 );
 	}
 
-	void serializeReponseEntity( final HttpServerExchange exchange, final Response response)
+	/**
+	 * Serialize and send the {@code response} object to the HTTP Client. The
+	 * HTTP Status Code will always be 200, in this case. Also, it will send use
+	 * <em>UTF-8</em> as default encoding.
+	 * 
+	 * @param exchange
+	 * @param contentType
+	 * @param response
+	 * @throws ServiceProviderException
+	 * @throws RoutingException
+	 */
+	public void write( final HttpServerExchange exchange, final String contentType, final Object response )
 			throws ServiceProviderException, RoutingException {
-		final Object serializable = response.entity();
+		sendStatusCode( exchange, 200 );
+		sendContentTypeHeader( exchange, contentType );
+		sendBodyResponse( exchange, contentType, "UTF-8", response );
+	}
+
+	/**
+	 * Serialize and send the {@code response} object to the HTTP Client.
+	 * 
+	 * @param exchange
+	 * @param response
+	 * @throws ServiceProviderException
+	 * @throws RoutingException
+	 * @see Response
+	 */
+	public void write( final HttpServerExchange exchange, final Response response )
+			throws ServiceProviderException, RoutingException {
+		sendStatusCode( exchange, response.statusCode() );
+		sendHeaders( exchange, response );
+		sendBodyResponse( exchange, response );
+	}
+
+	void sendBodyResponse( final HttpServerExchange exchange, final Response response )
+			throws ServiceProviderException, RoutingException {
+		sendBodyResponse( exchange,
+				response.contentType(), response.encoding(), response.entity() );
+	}
+
+	void sendBodyResponse(
+			final HttpServerExchange exchange, final String contentType,
+			final String encoding, final Object serializable )
+			throws ServiceProviderException, RoutingException {
 		final StreamSinkChannel channel = exchange.getResponseChannel();
-		final Writer writer = Channels.newWriter(channel, response.encoding());
-		final Serializer serializer  = provider.load( Serializer.class , response.contentType() );
-		serializer.serialize( serializable, writer);
+		final Writer writer = Channels.newWriter( channel, encoding );
+		final Serializer serializer = provider.load( Serializer.class, contentType );
+		serializer.serialize( serializable, writer );
 	}
 
 	void sendHeaders( final HttpServerExchange exchange, final Response response ) {
-		final HeaderMap responseHeaders = exchange.getResponseHeaders();
-		responseHeaders.add( new HttpString( Headers.CONTENT_TYPE_STRING ), response.contentType() );
+		final HeaderMap responseHeaders = sendContentTypeHeader( exchange, response.contentType() );
 		for ( final Header header : response.headers() )
 			for ( final String value : header.values() )
-				responseHeaders.add( new HttpString( header.name() ), value);
+				responseHeaders.add( new HttpString( header.name() ), value );
+	}
+
+	HttpServerExchange sendStatusCode( final HttpServerExchange exchange, Integer statusCode ) {
+		exchange.setResponseCode( statusCode );
+		return exchange;
+	}
+
+	HeaderMap sendContentTypeHeader( final HttpServerExchange exchange, String contentType ) {
+		final HeaderMap responseHeaders = exchange.getResponseHeaders();
+		responseHeaders.add( new HttpString( Headers.CONTENT_TYPE_STRING ), contentType );
+		return responseHeaders;
 	}
 }
