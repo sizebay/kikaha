03260e5f9be4e84ed6122d1d454b7410d11fb504
Miere Teixeira <miere00@gmail.com>
2014-06-18 17:48:58 -0300

    SampleRoute.java renamed to RetrieveRoutes.java

---
+++ undertow-urouting/tests/io/skullabs/undertow/routing/samples/RetrieveRoutes.java
@@ -0,0 +1,28 @@
+package io.skullabs.undertow.routing.samples;
+
+import urouting.api.*;
+
+@Path( "{contentType}/serasa" )
+public class RetrieveRoutes {
+
+	@GET
+	@Path( "relatomais" )
+	public void renderRelatoMais(
+			@HeaderParam( "Authorization-Token" ) Long authorizationToken,
+			@CookieParam( "SESSIONID" ) Integer sessionId,
+			@PathParam( "contentType" ) String contentType,
+			@QueryParam( "id" ) Double id ) {
+
+	}
+
+	@GET
+	@Path( "relatomais-xml" )
+	@Produces( "application/json" )
+	public String renderRelatoMaisXML(
+			@HeaderParam( "Authorization-Token" ) Long authorizationToken,
+			@CookieParam( "SESSIONID" ) Integer sessionId,
+			@PathParam( "contentType" ) String contentType,
+			@QueryParam( "id" ) Double id ) {
+		return "RelatoMais";
+	}
+}
