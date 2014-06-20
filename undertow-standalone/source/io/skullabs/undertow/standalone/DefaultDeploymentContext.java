161f6a355ee2947c075100a7149298d6272df9c9
Miere Teixeira <miere00@gmail.com>
2014-06-20 18:21:58 -0300

    Temporary workaround for https://issues.jboss.org/browse/UNDERTOW-267

--- undertow-standalone/source/io/skullabs/undertow/standalone/DefaultDeploymentContext.java
+++ undertow-standalone/source/io/skullabs/undertow/standalone/DefaultDeploymentContext.java
@@ -1,16 +1,12 @@
 package io.skullabs.undertow.standalone;
 
-import io.skullabs.undertow.standalone.api.DeploymentContext;
-import io.skullabs.undertow.standalone.api.DeploymentHook;
-import io.skullabs.undertow.standalone.api.RequestHook;
+import io.skullabs.undertow.standalone.api.*;
 import io.undertow.Handlers;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.RoutingHandler;
 import io.undertow.util.Methods;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -19,65 +15,58 @@ import lombok.extern.java.Log;
 
 @Log
 @Getter
-@Accessors( fluent=true )
+@Accessors( fluent = true )
 @RequiredArgsConstructor
 public class DefaultDeploymentContext implements DeploymentContext {
 
 	final Iterable<DeploymentHook> deploymentHooks;
 	final List<RequestHook> requestHooks;
-	final RoutingHandler uris = Handlers.routing();
-	final UndertowRoutedResourcesHook undertowRoutedResources = UndertowRoutedResourcesHook.wrap(uris);
+	final RoutingHandler rootHandler = Handlers.routing();
 	final Map<String, Object> attributes = new HashMap<String, Object>();
-	
-	/**
-	 * After deployment {@code undertowRoutedResources} will be populated with
-	 * resources that <i>undertow</i> are capable to route. Lets memorize a hook
-	 * with those resources to be called by the {@code RequestHookChain} during requests.
-	 *  
-	 * @return
-	 */
-	public DeploymentContext registerUndertowRoutedResourcesHook(){
-		requestHooks.add(undertowRoutedResources);
+
+	@Override
+	public DeploymentContext register( RequestHook hook ) {
+		this.requestHooks.add( hook );
 		return this;
 	}
 
 	@Override
-	public DeploymentContext register(RequestHook hook) {
-		this.requestHooks.add(hook);
-		return this;
+	public DeploymentContext register( String uri, HttpHandler handler ) {
+		return register( uri, Methods.GET_STRING, handler );
 	}
 
 	@Override
-	public DeploymentContext register(String uri, HttpHandler handler) {
-		return register(uri, Methods.GET_STRING, handler);
-	}
-
-	@Override
-	public DeploymentContext register(String uri, String method, HttpHandler handler) {
+	public DeploymentContext register( String uri, String method, HttpHandler handler ) {
 		log.info( "Registering route: " + method + ":" + uri + "." );
-		this.uris.add(method, uri, handler);
+		this.rootHandler.add( method, uri, handler );
 		return this;
 	}
 
 	@Override
-	public <T> DeploymentContext attribute(Class<T> clazz, T object) {
+	public <T> DeploymentContext attribute( Class<T> clazz, T object ) {
 		return attribute( clazz.getCanonicalName(), object );
 	}
 
 	@Override
-	public DeploymentContext attribute(String key, Object object) {
-		attributes.put(key, object);
+	public DeploymentContext attribute( String key, Object object ) {
+		attributes.put( key, object );
 		return this;
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")
-	public <T> T attribute(Class<T> clazz) {
-		return (T)attribute(clazz.getCanonicalName());
+	@SuppressWarnings( "unchecked" )
+	public <T> T attribute( Class<T> clazz ) {
+		return (T)attribute( clazz.getCanonicalName() );
 	}
 
 	@Override
-	public Object attribute(String key) {
-		return attributes.get(key);
+	public Object attribute( String key ) {
+		return attributes.get( key );
+	}
+
+	@Override
+	public DeploymentContext fallbackHandler( HttpHandler fallbackHandler ) {
+		rootHandler.setFallbackHandler( fallbackHandler );
+		return this;
 	}
 }
