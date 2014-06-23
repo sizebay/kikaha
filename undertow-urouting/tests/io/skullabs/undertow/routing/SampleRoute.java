9dc56d11a58e6b6b66e84902fa87fe5358a10011
Miere Teixeira <miere00@gmail.com>
2014-06-23 11:22:45 -0300

    Added a unit test to be used in development time. Useful only for manual testing. Should keep it ignored when commit it.

---
+++ undertow-urouting/tests/io/skullabs/undertow/routing/SampleRoute.java
@@ -0,0 +1,22 @@
+package io.skullabs.undertow.routing;
+
+import io.skullabs.undertow.urouting.Mimes;
+import io.skullabs.undertow.urouting.api.*;
+import trip.spi.Service;
+
+@Service
+@Path( "sample" )
+public class SampleRoute {
+
+	@GET
+	public String printUser() {
+		return "User";
+	}
+
+	@GET
+	@Path( "json" )
+	@Produces( Mimes.JSON )
+	public User printUserAsJSON() {
+		return new User( "Handom Wser" );
+	}
+}
