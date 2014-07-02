f669ad14a1313e66709e6fe055a2ac44709b656f
Miere Liniel Teixeira <miere00@gmail.com>
2014-07-02 18:42:09 -0300

    Added default values to DefaultResponse

--- undertow-urouting/source/io/skullabs/undertow/urouting/api/DefaultResponse.java
+++ undertow-urouting/source/io/skullabs/undertow/urouting/api/DefaultResponse.java
@@ -4,21 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 
 @Getter
+@Setter
 @Accessors( fluent=true )
 @NoArgsConstructor
 @RequiredArgsConstructor
 public class DefaultResponse implements Response {
 
 	@NonNull Object entity;
-	@NonNull Integer statusCode;
-	@NonNull String encoding;
-	@NonNull String contentType;
+	@NonNull Integer statusCode = 200;
+	@NonNull String encoding = "UTF-8";
+	@NonNull String contentType = Mimes.PLAIN_TEXT;
 	@NonNull List<Header> headers = new ArrayList<>();
 
 	public DefaultResponse header( String name, String value ) {
