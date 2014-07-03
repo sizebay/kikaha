dd53bdf544f9e03b0ebe0b5c65750d8b01e1d7e1
Miere Teixeira <miere00@gmail.com>
2014-07-03 11:44:20 -0300

    Added some javadocs.

--- undertow-hazelcast/source/io/skullabs/undertow/hazelcast/HazelcastInstanceProducer.java
+++ undertow-hazelcast/source/io/skullabs/undertow/hazelcast/HazelcastInstanceProducer.java
@@ -6,15 +6,30 @@ import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import io.skullabs.undertow.hazelcast.HazelcastConfiguration.ClusterClientConfig;
+import lombok.extern.java.Log;
 import trip.spi.*;
 
+@Log
 @Service
 public class HazelcastInstanceProducer {
+
+	@Provided
+	ServiceProvider provider;
 
 	@Provided
 	HazelcastConfiguration hazelcastConfig;
 	HazelcastInstance instance;
 
+	/**
+	 * Produce a Hazelcast Instance. It will ensure that has only one instance
+	 * of Hazelcast. You are always able to "inject"
+	 * {@code HazelcastInstanceProducer} and call
+	 * {@code createHazelcastInstance()} method manually to produce more
+	 * Hazelcast instances.
+	 * 
+	 * @return a HazelcastInstance
+	 * @see HazelcastInstanceProducer#createHazelcastInstance()
+	 */
 	@Producer
 	public HazelcastInstance produceHazelcastInstance() {
 		if ( instance == null )
@@ -22,13 +37,37 @@ public class HazelcastInstanceProducer {
 		return instance;
 	}
 
-	HazelcastInstance createHazelcastInstance() {
+	/**
+	 * Creates a Hazelcast Instance. If the hazelcast mode, defined in
+	 * {@code application.conf}, is set to "client", it will creates an instance
+	 * that connects to a cluster node, otherwise, it will create an instance
+	 * that behaves like a cluster node.<br>
+	 * <br>
+	 * 
+	 * <b>Note:</b> you are always able to provide your own {@link Config} or
+	 * {@link ClientConfig} implementation through tRip producer.
+	 * 
+	 * @return a HazelcastInstance
+	 */
+	public HazelcastInstance createHazelcastInstance() {
 		if ( hazelcastConfig.mode().equals( HazelcastConfiguration.MODE_CLIENT ) ) {
-			final ClientConfig clientConfig = createClientConfiguration();
+			final ClientConfig clientConfig = loadClientConfig();
 			return HazelcastClient.newHazelcastClient( clientConfig );
 		}
-		final Config cfg = new Config();
+		final Config cfg = loadConfig();
 		return Hazelcast.newHazelcastInstance( cfg );
+	}
+
+	ClientConfig loadClientConfig() {
+		try {
+			ClientConfig clientConfig = provider.load( ClientConfig.class );
+			if ( clientConfig == null )
+				clientConfig = createClientConfiguration();
+			return clientConfig;
+		} catch ( ServiceProviderException cause ) {
+			log.warning( "Could not read Hazelcast Client Configuration: " + cause.getMessage() + ". Creating one manually." );
+			return createClientConfiguration();
+		}
 	}
 
 	ClientConfig createClientConfiguration() {
@@ -45,5 +84,21 @@ public class HazelcastInstanceProducer {
 
 	boolean isBlank( String string ) {
 		return string == null || string.isEmpty();
+	}
+
+	Config loadConfig() {
+		try {
+			Config config = provider.load( Config.class );
+			if ( config == null )
+				config = createConfig();
+			return config;
+		} catch ( ServiceProviderException cause ) {
+			log.warning( "Could not read Hazelcast Configuration: " + cause.getMessage() + ". Creating one manually." );
+			return createConfig();
+		}
+	}
+
+	private Config createConfig() {
+		return new Config();
 	}
 }
