52d1c64565cc7092f1965885aa4489e16f134a8d
Miere Teixeira <miere00@gmail.com>
2014-07-03 10:53:37 -0300

    Optimizing the normalization of relativePath ( due to unresolved issue https://issues.jboss.org/browse/UNDERTOW-267 )

--- undertow-standalone/source/io/skullabs/undertow/standalone/UndertowRoutedResourcesHook.java
+++ undertow-standalone/source/io/skullabs/undertow/standalone/UndertowRoutedResourcesHook.java
@@ -21,8 +21,19 @@ public class UndertowRoutedResourcesHook implements RequestHook {
 		}
 	}
 
-	private void fixRelativePath( HttpServerExchange exchange ) {
-		String relativePath = exchange.getRelativePath();
-		exchange.setRelativePath( relativePath.replaceFirst( "/+$", "" ) );
+	void fixRelativePath( HttpServerExchange exchange ) {
+		final String relativePath = removeTrailingCharacter( exchange.getRelativePath() );
+		exchange.setRelativePath( relativePath );
+	}
+
+	String removeTrailingCharacter( String original ) {
+		final StringBuilder builder = new StringBuilder( original );
+		while ( hasRemaningTrailingCharacter( builder ) )
+			builder.deleteCharAt( builder.length() - 1 );
+		return builder.toString();
+	}
+
+	boolean hasRemaningTrailingCharacter( final StringBuilder builder ) {
+		return builder != null && builder.length() > 1 && '/' == builder.charAt( builder.length() - 1 );
 	}
 }
