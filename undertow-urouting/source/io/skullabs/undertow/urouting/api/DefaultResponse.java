fd95f45325f6fb5312d673126f2d9bd658fd1fef
Miere Liniel Teixeira <miere00@gmail.com>
2014-07-07 18:09:23 -0300

    Improved DefaultResponse API

--- undertow-urouting/source/io/skullabs/undertow/urouting/api/DefaultResponse.java
+++ undertow-urouting/source/io/skullabs/undertow/urouting/api/DefaultResponse.java
@@ -1,5 +1,6 @@
 package io.skullabs.undertow.urouting.api;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,5 +39,57 @@ public class DefaultResponse implements Response {
 			if ( header.name().equals(name) )
 				return header;
 		return null;
+	}
+	
+	private static DefaultResponse response() {
+		return new DefaultResponse();
+	}
+
+	public static DefaultResponse ok() {
+		return response().statusCode(200);
+	}
+
+	public static DefaultResponse ok( Object entity ) {
+		return ok().entity(entity);
+	}
+
+	public static DefaultResponse noContent() {
+		return response().statusCode(204);
+	}
+
+	public static DefaultResponse notModified() {
+		return response().statusCode(304);
+	}
+
+	public static DefaultResponse seeOther() {
+		return response().statusCode(303);
+	}
+
+	public static DefaultResponse seeOther( String location ) {
+		return response().statusCode(303)
+				.header("Location", location);
+	}
+
+	public static DefaultResponse serverError() {
+		return response().statusCode(500);
+	}
+
+	public static DefaultResponse serverError( String string ) {
+		return response().statusCode(500)
+				.entity(string);
+	}
+
+	public static DefaultResponse temporaryRedirect() {
+		return response().statusCode(307);
+	}
+
+	public static DefaultResponse temporaryRedirect( String location ) {
+		return response().statusCode(307)
+				.header("Location", location);
+	}
+
+	public static DefaultResponse temporaryRedirect( URI location ) {
+		return response().statusCode(307)
+				.header("Location", location.toString());
 	}
 }
