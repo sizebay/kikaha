76ba195bb79fe604760f1bcd76756c13d6728a2f
Miere Teixeira <miere00@gmail.com>
2014-07-10 18:27:21 -0300

    Updated Lombok version

--- undertow-urouting/source/io/skullabs/undertow/urouting/UncloseableWriterWrapper.java
+++ undertow-urouting/source/io/skullabs/undertow/urouting/UncloseableWriterWrapper.java
@@ -3,8 +3,8 @@ package io.skullabs.undertow.urouting;
 import java.io.Closeable;
 import java.io.Writer;
 
-import lombok.Delegate;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.Delegate;
 
 @RequiredArgsConstructor( staticName="wrap" )
 public class UncloseableWriterWrapper extends Writer {
