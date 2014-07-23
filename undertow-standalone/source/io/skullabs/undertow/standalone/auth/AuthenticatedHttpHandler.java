2b6033ac79aa493ebb9fca4041584886114e33ef
Miere Teixeira <miere00@gmail.com>
2014-07-23 16:30:27 -0300

    Initial authentication support implementation

---
+++ undertow-standalone/source/io/skullabs/undertow/standalone/auth/AuthenticatedHttpHandler.java
@@ -0,0 +1,48 @@
+package io.skullabs.undertow.standalone.auth;
+
+import io.undertow.security.api.AuthenticationMechanism;
+import io.undertow.security.api.AuthenticationMode;
+import io.undertow.security.api.SecurityContext;
+import io.undertow.security.api.SecurityContextFactory;
+import io.undertow.security.idm.IdentityManager;
+import io.undertow.security.impl.SecurityContextFactoryImpl;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+
+@RequiredArgsConstructor
+public class AuthenticatedHttpHandler implements HttpHandler {
+
+	final SecurityContextHandler securityContextHandler = SecurityContextHandler.DEFAULT;
+	final SecurityContextFactory contextFactory = SecurityContextFactoryImpl.INSTANCE;
+	final IdentityManager identityManager;
+	final List<AuthenticationMechanism> authenticationMechanisms;
+	final HttpHandler authenticatedRoute;
+
+	@Override
+	public void handleRequest( HttpServerExchange exchange ) throws Exception {
+		val context = createSecurityContext( exchange );
+		populateWithAuthenticationMechanisms( context );
+		context.setAuthenticationRequired();
+		if ( !context.authenticate() )
+			exchange.endExchange();
+		else
+			authenticatedRoute.handleRequest( exchange );
+	}
+
+	SecurityContext createSecurityContext( final HttpServerExchange exchange ) {
+		val newContext = this.contextFactory.createSecurityContext(
+				exchange, AuthenticationMode.PRO_ACTIVE, identityManager, null );
+		securityContextHandler.setSecurityContext( exchange, newContext );
+		return newContext;
+	}
+
+	void populateWithAuthenticationMechanisms( final SecurityContext context ) {
+		for ( val authenticationMechanism : authenticationMechanisms )
+			context.addAuthenticationMechanism( authenticationMechanism );
+	}
+}
---
+++ undertow-standalone/tests/io/skullabs/undertow/standalone/auth/AuthenticatedHttpHandlerTest.java
@@ -0,0 +1,79 @@
+package io.skullabs.undertow.standalone.auth;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import io.skullabs.undertow.standalone.HttpServerExchangeStub;
+import io.undertow.security.api.AuthenticationMechanism;
+import io.undertow.security.api.SecurityContext;
+import io.undertow.security.idm.IdentityManager;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.val;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class AuthenticatedHttpHandlerTest {
+
+	@Mock
+	IdentityManager identityManager;
+
+	@Mock
+	AuthenticationMechanism authMechanism;
+
+	@Mock
+	HttpHandler httpHandler;
+
+	@Mock
+	SecurityContext securityContext;
+
+	@Before
+	public void initializeMocks() {
+		MockitoAnnotations.initMocks( this );
+	}
+
+	@Test
+	public void ensureThatAreAbleToByPassAllAuthenticationMechanismsToSecurityContext() throws Exception {
+		val exchange = HttpServerExchangeStub.createHttpExchange();
+		val authHandler = createMockedAuthenticatedHandlerFor( exchange );
+		authHandler.handleRequest( exchange );
+		verify( securityContext ).addAuthenticationMechanism( authMechanism );
+	}
+
+	@Test
+	public void ensureThatSetAuthenticationAsRequired() throws Exception {
+		val exchange = HttpServerExchangeStub.createHttpExchange();
+		val authHandler = createMockedAuthenticatedHandlerFor( exchange );
+		authHandler.handleRequest( exchange );
+		verify( securityContext ).setAuthenticationRequired();
+	}
+
+	@Test
+	public void ensureThatCouldCallTheTargetHttpHandlerWhenIsAuthenticated() throws Exception {
+		val exchange = HttpServerExchangeStub.createHttpExchange();
+		val authHandler = createMockedAuthenticatedHandlerFor( exchange );
+		when( securityContext.authenticate() ).thenReturn( true );
+		authHandler.handleRequest( exchange );
+		verify( httpHandler ).handleRequest( exchange );
+	}
+
+	AuthenticatedHttpHandler createMockedAuthenticatedHandlerFor( final HttpServerExchange exchange ) {
+		val authMechs = createListOfAuthenticationMechanisms();
+		val authHandler = spy( new AuthenticatedHttpHandler( identityManager, authMechs, httpHandler ) );
+		when( authHandler.createSecurityContext( exchange ) ).thenReturn( securityContext );
+		return authHandler;
+	}
+
+	List<AuthenticationMechanism> createListOfAuthenticationMechanisms() {
+		val authMechs = new ArrayList<AuthenticationMechanism>();
+		authMechs.add( authMechanism );
+		return authMechs;
+	}
+}
