16547e778da2173e2992a079f8243428abe41705
Miere Teixeira <miere00@gmail.com>
2014-09-21 19:43:06 -0300

    Applied java code formater

--- kikaha-urouting/source/kikaha/urouting/serializers/XMLSerializer.java
+++ kikaha-urouting/source/kikaha/urouting/serializers/XMLSerializer.java
@@ -14,29 +14,29 @@ import kikaha.urouting.api.RoutingException;
 import kikaha.urouting.api.Serializer;
 import trip.spi.Singleton;
 
-@SuppressWarnings("unchecked")
+@SuppressWarnings( "unchecked" )
 @Singleton( name = Mimes.XML, exposedAs = Serializer.class )
 public class XMLSerializer implements Serializer {
 
 	@Override
-	public <T> void serialize(T object, Writer output) throws RoutingException {
-        try {
-			Class<T> clazz = (Class<T>) object.getClass();
-			serialize(clazz, object, output);
-		} catch (JAXBException cause) {
-			throw new RoutingException(cause);
+	public <T> void serialize( final T object, final Writer output ) throws RoutingException {
+		try {
+			final Class<T> clazz = (Class<T>)object.getClass();
+			serialize( clazz, object, output );
+		} catch ( final JAXBException cause ) {
+			throw new RoutingException( cause );
 		}
 	}
 
-	<T> void serialize(Class<T> clazz, T object, Writer output) throws JAXBException {
+	<T> void serialize( final Class<T> clazz, final T object, final Writer output ) throws JAXBException {
 		final JAXBContext context = JAXBContext.newInstance( clazz );
 		final String rootElementName = extractRootElementName( clazz );
-		final JAXBElement<T> element = new JAXBElement<T>( new QName(rootElementName), clazz, object );
+		final JAXBElement<T> element = new JAXBElement<T>( new QName( rootElementName ), clazz, object );
 		final Marshaller marshaller = context.createMarshaller();
-        marshaller.marshal( element, output );
+		marshaller.marshal( element, output );
 	}
 
-	String extractRootElementName( Class<?> clazz ) {
+	String extractRootElementName( final Class<?> clazz ) {
 		final XmlRootElement rootElement = clazz.getAnnotation( XmlRootElement.class );
 		if ( rootElement != null && !rootElement.name().isEmpty() )
 			return rootElement.name();
