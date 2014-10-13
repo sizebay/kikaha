87c11135f467168e1b3a4c41964e707362c77126
Miere Teixeira <miere00@gmail.com>
2014-10-13 13:53:45 -0300

    Added "text/html" entry to Mimes API

--- kikaha-urouting/source/kikaha/urouting/api/Mimes.java
+++ kikaha-urouting/source/kikaha/urouting/api/Mimes.java
@@ -3,7 +3,8 @@ package kikaha.urouting.api;
 public interface Mimes {
 
 	public static final String PLAIN_TEXT = "text/plain";
-	public static final String XML = "text/xml";
+	public static final String XML = "application/xml";
+	public static final String XML_LEGACY = "text/xml";
+	public static final String HTML = "text/html";
 	public static final String JSON = "application/json";
-
 }
