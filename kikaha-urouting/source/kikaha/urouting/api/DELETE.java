8f79d6f91f096b6f3b5f5e92c7c9813ee372202c
Miere Liniel Teixeira <miere00@gmail.com>
2015-03-02 14:42:32 -0300

    Closes #47

--- kikaha-urouting/source/kikaha/urouting/api/DELETE.java
+++ kikaha-urouting/source/kikaha/urouting/api/DELETE.java
@@ -1,10 +1,12 @@
 package kikaha.urouting.api;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Inherited
 @Target( { ElementType.METHOD, ElementType.TYPE } )
 @Retention( RetentionPolicy.RUNTIME )
 public @interface DELETE {
