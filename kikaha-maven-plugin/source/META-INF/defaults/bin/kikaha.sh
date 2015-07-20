7f80a1651cd7cbb566074733e55acecfe335b697
unknown <miere00@gmail.com>
2015-07-20 16:21:07 -0300

    Better (but simple) start/stop script for Kikaha

--- kikaha-maven-plugin/source/META-INF/defaults/bin/kikaha.sh
+++ kikaha-maven-plugin/source/META-INF/defaults/bin/kikaha.sh
@@ -8,6 +8,40 @@ LIBDIR="./lib"
 JAVA=java
 JAVA_OPTS=
 MAIN_CLASS=kikaha.core.Main
+OUTPUTFILE=server.stdout.log
+PIDFILE=server.pid
+
+start_server(){
+	nohup ${JAVA} ${JAVA_OPTS} -classpath "${CLASSPATH}" ${MAIN_CLASS} > $OUTPUTFILE &
+	echo "$!" > $PIDFILE
+}
+
+stop_server(){
+	if [ -e $PIDFILE ]; then
+		local PID=`cat $PIDFILE`
+		local FOUNDPROCS=`ps aux | grep $PID | head -n 1 | grep java`
+		if [ ! "$FOUNDPROCS" = "" ]; then
+			kill $PID && echo "kill term sent."
+			rm -f $PIDFILE
+		fi
+	else
+		echo "Server not running"
+		exit 1
+	fi
+}
+
+show_help(){
+	echo "Usage: $0 <command>"
+	echo
+	echo "Available commands:"
+	echo " - start:	starts the server in background"
+	echo " - stop:	stops the server that is running in background"
+	echo " - debug: run the server in foreground"
+	echo
+	echo "When no command is set, it starts the server in background by default"
+	echo
+
+}
 
 # READ CUSTOM CONFIGURATIONS
 if [ -e bin/kikaha.conf ]; then
@@ -16,4 +50,11 @@ fi
 
 # MAIN
 CLASSPATH=".:${LIBDIR}/*"
-${JAVA} ${JAVA_OPTS} -classpath "${CLASSPATH}" ${MAIN_CLASS}
+
+case "$1" in
+	"help" ) show_help ;;
+	"stop" ) stop_server ;;
+	"debug" ) ${JAVA} ${JAVA_OPTS} -classpath "${CLASSPATH}" ${MAIN_CLASS} ;;
+	"start" | "*" ) start_server ;;
+esac
+
