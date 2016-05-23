e7c116fe4b2de2b312bc0fd3f6785ea2cce998d5
Miere Liniel Teixeira <miere00@gmail.com>
2016-05-23 13:24:28 -0300

    Fixed URL remotion of trailing slashes

--- kikaha-core/source/kikaha/core/url/URL.java
+++ kikaha-core/source/kikaha/core/url/URL.java
@@ -5,6 +5,6 @@ public abstract class URL {
 	public static String removeTrailingCharacter( final String original ) {
 		int offset = original.length() - 1;
 		for ( ; original.charAt( offset ) == '/'; offset-- );
-		return original.substring(0, offset);
+		return original.substring(0, offset + 1);
 	}
 }
