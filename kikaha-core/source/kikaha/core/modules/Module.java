7ceeffc961dd31903dd52988622dc6b7a09ef888
Miere Liniel Teixeira <miere00@gmail.com>
2016-06-07 18:32:57 -0300

    Added shutdown hook for Modules

--- kikaha-core/source/kikaha/core/modules/Module.java
+++ kikaha-core/source/kikaha/core/modules/Module.java
@@ -1,17 +1,41 @@
 package kikaha.core.modules;
 
+import java.io.IOException;
 import io.undertow.Undertow;
 import kikaha.core.DeploymentContext;
 
-import java.io.IOException;
-
 /**
- *
+ * Defines a module that should be executed when the application is started or
+ * shutdown.
  */
 public interface Module {
 
+	/**
+	 * Retrieve the module name. It is useful for module ordering proposes only.
+	 *
+	 * @return the module name.
+	 */
 	String getName();
 
+	/**
+	 * Event executed when server is initializing. This event allow developers to enhance
+	 * the current application.<br>
+	 * <br>
+	 * The {@code context} instance allows deploy {@link io.undertow.server.HttpHandler}s,
+	 * {@link kikaha.core.modules.smart.Filter}s or other Smart Route.<br>
+	 * <br>
+	 * The {@code server} instance allow developers to customize the {@link Undertow} server
+	 * before it is started.
+	 *
+	 * @param server the server builder instance
+	 * @param context the deployment context instance
+	 * @throws IOException
+	 */
 	void load( Undertow.Builder server, DeploymentContext context ) throws IOException;
 
+	/**
+	 * Event executed when the server is shutdown. Allow developers to execute
+	 * graceful-shutdown routines avoiding data loss when the server needs to shutdown.
+	 */
+	default void unload(){}
 }
--- kikaha-core/source/kikaha/core/modules/ModuleLoader.java
+++ kikaha-core/source/kikaha/core/modules/ModuleLoader.java
@@ -1,21 +1,17 @@
 package kikaha.core.modules;
 
+import static java.util.Collections.reverse;
+import java.io.IOException;
+import java.util.*;
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Typed;
+import javax.inject.*;
 import io.undertow.Undertow;
 import kikaha.config.Config;
 import kikaha.core.DeploymentContext;
 
-import javax.enterprise.inject.Typed;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static java.util.Collections.reverse;
-
 /**
- *
+ * Load and manage {@link Module}s life cycle.
  */
 @Singleton
 public class ModuleLoader {
@@ -29,6 +25,11 @@ public class ModuleLoader {
 
 	Map<String, Module> modulesIndexedByName;
 	List<String> enabledModules;
+
+	@PostConstruct
+	public void memorizeShutdownHooks(){
+		Runtime.getRuntime().addShutdownHook( new Thread( this::unloadModules ) );
+	}
 
 	public void load( Undertow.Builder builder, DeploymentContext context ) throws IOException {
 		loadModulesConfigurations();
@@ -60,5 +61,10 @@ public class ModuleLoader {
 		for (Module module : modules)
 			index.put( module.getName(), module );
 		return index;
+	}
+
+	private void unloadModules(){
+		for ( Module module : modules )
+			module.unload();
 	}
 }
