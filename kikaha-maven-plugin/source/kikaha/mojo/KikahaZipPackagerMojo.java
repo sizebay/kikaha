2c8c40bdbeeaefe5837e1b4bbbc20106a111ad25
Miere <miere00@gmail.com>
2017-02-19 23:04:16 -0300

    Fixed issue on kikaha:package's maven goal

--- kikaha-maven-plugin/source/kikaha/mojo/KikahaZipPackagerMojo.java
+++ kikaha-maven-plugin/source/kikaha/mojo/KikahaZipPackagerMojo.java
@@ -1,28 +1,18 @@
 package kikaha.mojo;
 
-import kikaha.mojo.packager.ZipFileReader;
-import kikaha.mojo.packager.ZipFileWriter;
-import lombok.Cleanup;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
-import org.apache.maven.artifact.resolver.ArtifactResolutionException;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.MavenProject;
-
 import java.io.*;
 import java.net.URL;
 import java.security.CodeSource;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import kikaha.mojo.packager.*;
+import lombok.Cleanup;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.*;
+import org.apache.maven.plugin.*;
+import org.apache.maven.plugins.annotations.*;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.project.MavenProject;
 
 @Mojo( name = "package",
 		requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME )
@@ -43,7 +33,7 @@ public class KikahaZipPackagerMojo extends AbstractMojo {
 	@Parameter( defaultValue = "${project.build.resources[0].directory}", required = true )
 	File resourceDirectory;
 
-	@Parameter( defaultValue = "${project.build.originalJarFileName}", required = true )
+	@Parameter( defaultValue = "${project.build.finalName}", required = true )
 	String finalName;
 
 	@Parameter( defaultValue = "false", required = true)
