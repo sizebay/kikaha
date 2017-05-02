79cbc4aa891c9deda03133daa0c62015d9a5f2a5
Miere <miere00@gmail.com>
2017-05-02 18:51:24 -0300

    Fixed AmazonELBServiceRegistryTest

--- kikaha-modules/kikaha-cloud-aws-alb/tests/kikaha/cloud/aws/alb/AmazonELBServiceRegistryTest.java
+++ kikaha-modules/kikaha-cloud-aws-alb/tests/kikaha/cloud/aws/alb/AmazonELBServiceRegistryTest.java
@@ -1,29 +1,33 @@
 package kikaha.cloud.aws.alb;
 
+import static kikaha.core.test.Exposed.expose;
 import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.initMocks;
 import java.io.IOException;
-import javax.inject.Inject;
-import com.amazonaws.ResponseMetadata;
+import com.amazonaws.*;
 import com.amazonaws.http.SdkHttpMetadata;
 import com.amazonaws.services.elasticloadbalancingv2.AmazonElasticLoadBalancing;
 import com.amazonaws.services.elasticloadbalancingv2.model.*;
+import kikaha.cloud.aws.ec2.AmazonEC2ClientProducer;
+import kikaha.cloud.aws.iam.*;
 import kikaha.cloud.smart.ServiceRegistry.ApplicationData;
-import kikaha.config.Config;
-import kikaha.core.test.KikahaRunner;
+import kikaha.config.*;
+import kikaha.core.cdi.CDI;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.*;
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * Unit tests for {@link AmazonELBServiceRegistry}.
  */
-@RunWith( KikahaRunner.class )
+@RunWith( MockitoJUnitRunner.class )
 public class AmazonELBServiceRegistryTest {
 
 	final ApplicationData applicationData = new ApplicationData( () -> "unit01", () -> "localhost", "unit01", "1.0", 9000, false);
+	final Config defaultConfiguration = ConfigLoader.loadDefaults();
+	final AmazonCredentialsFactory.Yml yml = new AmazonCredentialsFactory.Yml().setConfig( defaultConfiguration );
 
-	@Inject AmazonELBServiceRegistry registry;
+	@Mock CDI cdi;
 	@Mock Config config;
 	@Mock AmazonElasticLoadBalancing client;
 	@Mock RegisterTargetsResult registerTargetsResult;
@@ -31,11 +35,27 @@ public class AmazonELBServiceRegistryTest {
 	@Mock ResponseMetadata sdkResponseMetadata;
 	@Mock SdkHttpMetadata sdkHttpMetadata;
 
+	AmazonELBServiceRegistry registry = new AmazonELBServiceRegistry();
+	ClientConfiguration clientConfiguration = new ClientConfiguration();
+	AmazonCredentialsProducer credentialsProducer = new AmazonCredentialsProducer();
+	AmazonConfigurationProducer configurationProducer = new AmazonConfigurationProducer();
+	AmazonEC2ClientProducer ec2ClientProducer = new AmazonEC2ClientProducer();
+
 	@Before
-	public void setupMocks(){
-		initMocks(this);
-		registry.config = config;
-		registry = Mockito.spy( registry );
+	public void configureMocks(){
+		doReturn( defaultConfiguration ).when( cdi ).load( eq(Config.class) );
+		doReturn( yml ).when( cdi ).load( eq( AmazonCredentialsFactory.Yml.class ) );
+
+		expose( credentialsProducer ).setFieldValue( "cdi", cdi );
+
+		expose( configurationProducer )
+			.setFieldValue( "cdi", cdi )
+			.setFieldValue( "credentialsProducer", credentialsProducer );
+
+		expose( ec2ClientProducer )
+			.setFieldValue("configurationProducer", configurationProducer)
+			.setFieldValue("configuration", clientConfiguration);
+
 
 		doReturn( client ).when( registry ).elbClient();
 		doReturn( registerTargetsResult ).when(client).registerTargets( any() );
@@ -44,6 +64,12 @@ public class AmazonELBServiceRegistryTest {
 		doReturn( sdkResponseMetadata ).when(deregisterTargetsResult).getSdkResponseMetadata();
 		doReturn( sdkHttpMetadata ).when(registerTargetsResult).getSdkHttpMetadata();
 		doReturn( sdkHttpMetadata ).when(deregisterTargetsResult).getSdkHttpMetadata();
+	}
+
+	@Before
+	public void setupMocks(){
+		registry.config = config;
+		registry = Mockito.spy( registry );
 	}
 
 	@Test( expected = IOException.class )
