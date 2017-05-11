a676e3d92bc512d4cf86061c1e88f2224c9e7ddc
Miere <miere00@gmail.com>
2017-05-11 08:58:49 -0300

    Renamed AmazonLocalMachineIdentification.java to AmazonEC2MachineIdentification.java

--- kikaha-modules/kikaha-cloud-aws-ec2/source/kikaha/cloud/aws/ec2/AmazonEC2MachineIdentification.java
+++ kikaha-modules/kikaha-cloud-aws-ec2/source/kikaha/cloud/aws/ec2/AmazonEC2MachineIdentification.java
@@ -5,27 +5,30 @@ import javax.inject.Singleton;
 import com.amazonaws.util.EC2MetadataUtils;
 import kikaha.cloud.smart.LocalMachineIdentification;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author: miere.teixeira
  */
+@Slf4j
 @Singleton
 public class AmazonLocalMachineIdentification implements LocalMachineIdentification {
 
 	@Getter(lazy = true)
 	private final String machineId = EC2MetadataUtils.getInstanceId();
 
-
 	@Getter(lazy = true)
 	private final String ipAddress = EC2MetadataUtils.getPrivateIpAddress();
 
 	@Override
 	public String generateTheMachineId() throws IOException {
+		log.info("Retrieving EC2 machine id...");
 		return getMachineId();
 	}
 
 	@Override
 	public String getLocalAddress() throws IOException {
+		log.info("Retrieving EC2 machine IP address...");
 		return getIpAddress();
 	}
 }
