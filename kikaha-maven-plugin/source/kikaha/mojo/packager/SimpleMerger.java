fc22b73a8d992175ddd5714f897d4f5f59c1b285
Miere <miere00@gmail.com>
2017-05-21 18:36:13 -0300

    Fixed META-INF/services merger

--- kikaha-maven-plugin/source/kikaha/mojo/packager/SimpleMerger.java
+++ kikaha-maven-plugin/source/kikaha/mojo/packager/SimpleMerger.java
@@ -1,11 +1,8 @@
 package kikaha.mojo.packager;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-import java.io.*;
-
 import static kikaha.mojo.packager.packager.copy;
+import java.io.*;
+import lombok.*;
 
 /**
  * Created by miere.teixeira on 06/12/2016.
@@ -20,6 +17,7 @@ public class SimpleMerger implements FileMerger {
     @Override
     public void add(InputStream inputStream) throws IOException {
         copy( inputStream, buffer::write );
+        copy( new ByteArrayInputStream( "\n".getBytes() ), buffer::write );
     }
 
     @Override
