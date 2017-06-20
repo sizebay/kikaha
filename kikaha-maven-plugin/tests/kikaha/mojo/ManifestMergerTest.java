c4258362ec0372133a670fafd944e0903aa75d2d
Miere <miere00@gmail.com>
2017-06-19 22:36:32 -0300

    Version 2.1.0-alpha22

--- kikaha-maven-plugin/tests/kikaha/mojo/ManifestMergerTest.java
+++ kikaha-maven-plugin/tests/kikaha/mojo/ManifestMergerTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 public class ManifestMergerTest {
 
     final String expectedMergedManifest = "Manifest-Version: 1.0\r\n" +
-            "Created-By: Kikaha 2.1.0-alpha21\r\n" +
+            "Created-By: Kikaha 2.1.0-alpha22\r\n" +
             "Main-Class: kikaha.core.cdi.ApplicationRunner\r\n" +
             "\r\n";
 
